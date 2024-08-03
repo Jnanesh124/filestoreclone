01408d699486ad19655c7d319b2a987880076dbf
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-08-03 18:16:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -14,7 +14,6 @@ from plugins.users_api import get_user, update_user_info
 from plugins.database import get_file_details
 from pyrogram.errors import *
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.types import *
 from utils import verify_user, check_token, check_verification, get_token
 from config import AUTH_CHANNEL
 import re
@@ -25,18 +24,6 @@ from TechVJ.utils.file_properties import get_name, get_hash, get_media_file_size
 logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
-
-async def is_subscribed(bot, query, channel):
-    btn = []
-    for id in channel:
-        chat = await bot.get_chat(int(id))
-        try:
-            await bot.get_chat_member(id, query.from_user.id)
-        except UserNotParticipant:
-            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
-        except Exception as e:
-            pass
-    return btn
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
