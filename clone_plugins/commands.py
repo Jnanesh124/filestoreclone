e4fb6a7ee70a8834739c86853bd4f7a05483886a
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-08-04 21:38:26 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -21,7 +21,7 @@ import json
 import base64
 from config import DB_URI as MONGO_URL
 from pymongo import MongoClient
-from utils1 import verify_user, check_token, check_verification, get_token
+from utiles1 import verify_user, check_token, check_verification, get_token
 
 async def is_subscribed(bot, query, channel):
     btn = []
