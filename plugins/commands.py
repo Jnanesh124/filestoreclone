d600f2906ec4d9ed1d39d42957c04b7f17aae6db
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-08-04 19:03:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -119,14 +119,14 @@ async def start(client, message):
     if AUTH_CHANNEL:
         try:
             btn = await is_subscribed(client, message, AUTH_CHANNEL)
-            if btn:  # Indentation fix here
-                username = (await client.get_me()).username
-                if message.command[1]:
-                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start={message.command[1]}")])
-                else:
-                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start=true")])
-                await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))
-                return
+                if btn:  # Indentation fix here
+                    username = (await client.get_me()).username
+                    if message.command[1]:
+                        btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start={message.command[1]}")])
+                    else:
+                        btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start=true")])
+                    await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))
+                    return
         except Exception as e:
             print(e)
         # ... Rest of your BATCH handling code ... 
